752c9e4dc58ea8ead1e2fe86115192ef7e624cd9
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-04-19 17:33:16 +0200

    HW02 update 2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import Contact from '../db/models/contact.js';
+import Contact from '../db/models/Contact.js';
 
 export const getContacts = () => Contact.find();
 export const getContactById = (contactId) =>
