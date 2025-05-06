ab1fea60efbdae39df1a8f2cc2fa966f1ce40ee4
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-05-06 23:16:16 +0200

    HW04 update 4

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    return next(createHttpError(404, `${id} not valid id`));
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    throw createHttpError(400, 'Bad request. Contact Id is not valid');
   }
   next();
 };
