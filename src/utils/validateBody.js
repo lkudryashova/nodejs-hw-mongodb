3aa0ee471447d2d2bc286297ab03b30dafe6f605
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-05-07 21:23:52 +0200

    HW04 update 5

--- src/utils/validateBody.js
+++ src/utils/validateBody.js
@@ -2,10 +2,12 @@ import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => {
   const func = async (req, res, next) => {
+    console.log('Body:', req.body);
     try {
       await schema.validateAsync(req.body, {
         abortEarly: false,
       });
+      next();
     } catch (error) {
       throw createHttpError(400, error.message);
     }
