20169f98048bf8644325881e3dc3a565613d2fe6
Lesya Kudryashova <lkudryashova.work@gmail.com>
2025-07-19 08:07:50 +0200

    HW06

--- src/validation/request-reset-password-email-validation-schema.js
+++ src/validation/request-reset-password-email-validation-schema.js
@@ -1,9 +1,5 @@
 import Joi from 'joi';
 
 export const requestResetPasswordEmailValidationSchema = Joi.object({
-<<<<<<< HEAD
   email: Joi.string().email().required(),
-=======
-  email: Joi.string().min(3).max(20),
->>>>>>> 12b1b4ef4fc1328476be33f38765f8e30058b799
 });
